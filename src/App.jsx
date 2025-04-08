8877fef325c916959ffe9debff5c080f53972f43
Madhavgarg98 <madhavgarg3300@gmail.com>
2025-04-08 09:21:03 +0530

    destinations

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DestinationContainer from './DestinationContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <DestinationContainer />
+    </div>
+  );
+}
+
+export default App;
